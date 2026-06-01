07ff98e777bfe1bad1d852edacf365d340f9867b
CPrutean <calin.prutean@gmail.com>
2026-06-01 11:41:29 -0700

    feat: created a servo frame object following the conventions from the protocol pdf

---
+++ include/can/Servo_frame.hpp
@@ -0,0 +1,62 @@
+#ifndef __SERVO_AK_SERIES
+#define __SERVO_AK_SERIES
+#pragma once
+
+#include "Exceptions.hpp"
+#include "frame.hpp"
+#include <stdexcept>
+#include <stdint.h>
+
+enum class ServoFrameID : uint8_t {
+  // int32_t(Set duty cycle voltage)
+  DutyCycleMode = 0,
+  // int32_t(Set current)
+  CurrentLoopMode,
+  // int32_t(How much current to brake)
+  CurrentBrakeMode,
+  // int32_t (Velocity value)
+  RPMmode,
+  // int32_t (Position value)
+  PositionMode,
+  // uint8_t (Send to origin value)
+  //  0 for setting current position to temp value
+  //  1 for setting the permanent 0 point (only on dual encoder devices)
+  SetOriginMode,
+  // int32_t(Position value), int16_t (speed value), int16_t (acceleration value)
+  PositionVelocityMode,
+  // This is the message servo mode screams back at you
+  // int16_t (position), int16_t (speed), int16)_t (current), uint8_t (motor temperature),
+  // uint8_t(error code)
+  ServoModeFeedback = 0x29
+};
+
+class ServoFrame : public Frame {
+private:
+  ServoFrameID m_servo_id;
+  ServoFrame(canid_t id, ServoFrameID frame_id);
+  int32_t m_frame_header{};
+  uint8_t get_id();
+  bool is0x29();
+
+public:
+  // when we read, we implicitly assume that it will be a canframe we are pulling this from
+  ServoFrame(can_frame frame);
+  [[nodiscard]] explicit operator can_frame();
+  [[nodiscard]] static ServoFrame setDutyCycle(canid_t can_id, float dutyCycle);
+  [[nodiscard]] static ServoFrame setCurrentLoop(canid_t can_id, int32_t currentLoop);
+  [[nodiscard]] static ServoFrame setCurrentBrake(canid_t can_id, int32_t current);
+  [[nodiscard]] static ServoFrame setRPM(canid_t can_id, int32_t rpm);
+  [[nodiscard]] static ServoFrame setPosition(canid_t can_id, int32_t pos);
+  [[nodiscard]] static ServoFrame setOrigin(canid_t can_id, uint8_t origin_mode);
+  [[nodiscard]] static ServoFrame setPositionAndVelo(canid_t, int32_t position, int16_t speed,
+                                                     int16_t accel);
+  // If the frame was constructed with the wrong can_frame struct
+  // This will throw, the command id needs to be 0x29
+  float getPosition();
+  int32_t getSpeed();
+  float getCurrent();
+  int8_t getTemperature();
+  ServoErrorCode getErrorCode();
+};
+
+#endif
---
+++ src/can/Servo_frame.cpp
@@ -0,0 +1,230 @@
+#include "can/Servo_frame.hpp"
+#include <charconv>
+#include <functional>
+#include <stdexcept>
+#include <string.h>
+
+static std::string invalidCallStr(uint8_t id) {
+  char buffer[10];
+  std::to_chars(&buffer[0], &buffer[9], id);
+  std::string desc{"Invalid call called get_current on a frame with id 0x29 this has id of "};
+  desc += buffer;
+  return desc;
+}
+
+ServoFrame::ServoFrame(canid_t id, ServoFrameID frame_id) : Frame(id), m_servo_id{frame_id} {
+  m_frame_header = static_cast<uint8_t>(m_servo_id) << 8;
+  m_frame_header += static_cast<int32_t>(id);
+}
+
+ServoFrame ServoFrame::setDutyCycle(canid_t can_id, float dutyCycle) {
+  // Look in the PDF to see why this is done
+  int32_t duty = static_cast<int32_t>(dutyCycle * 100000);
+  ServoFrame f(can_id, ServoFrameID::DutyCycleMode);
+
+  f.m_data[0] = static_cast<uint8_t>(duty >> 24);
+  f.m_data[1] = static_cast<uint8_t>(duty >> 16);
+  f.m_data[2] = static_cast<uint8_t>(duty >> 8);
+  f.m_data[3] = static_cast<uint8_t>(duty);
+
+  return f;
+}
+
+ServoFrame ServoFrame::setCurrentLoop(canid_t can_id, int32_t current) {
+  ServoFrame f(can_id, ServoFrameID::CurrentLoopMode);
+
+  [[unlikely]]
+  if (current < -60000 || current > 60000) {
+    current = current < -60000 ? -60000 : 60000;
+    char buf[500]{};
+    ::snprintf(buf, sizeof(buf) - 1,
+               "Invalid argument, you sent a value of %d but the limits are -60000, 60000",
+               current);
+
+    throw std::invalid_argument(const_cast<const char *>(buf));
+  }
+
+  f.m_data[0] = static_cast<uint8_t>(current >> 24);
+  f.m_data[1] = static_cast<uint8_t>(current >> 16);
+  f.m_data[2] = static_cast<uint8_t>(current >> 8);
+  f.m_data[3] = static_cast<uint8_t>(current);
+
+  return f;
+}
+
+ServoFrame ServoFrame::setCurrentBrake(canid_t can_id, int32_t current) {
+  ServoFrame f(can_id, ServoFrameID::CurrentBrakeMode);
+
+  if (current < 0 || current > 60000) {
+
+    char buf[500]{};
+    ::snprintf(buf, sizeof(buf) - 1,
+               "Invalid argument, you sent a value of %d but the limits are 0, 60000", current);
+
+    throw std::invalid_argument(buf);
+  }
+
+  f.m_data[0] = static_cast<uint8_t>(current >> 24);
+  f.m_data[1] = static_cast<uint8_t>(current >> 16);
+  f.m_data[2] = static_cast<uint8_t>(current >> 8);
+  f.m_data[3] = static_cast<uint8_t>(current);
+
+  return f;
+}
+
+ServoFrame ServoFrame::setRPM(canid_t can_id, int32_t rpm) {
+  ServoFrame f(can_id, ServoFrameID::RPMmode);
+
+  if (rpm < -100000 || rpm > 100000) {
+    char buf[500]{};
+    ::snprintf(buf, sizeof(buf) - 1,
+               "Invalid argument, you sent a value of %d but the limits are -10000, 10000", rpm);
+
+    throw std::invalid_argument(buf);
+  }
+
+  f.m_data[0] = static_cast<uint8_t>(rpm >> 24);
+  f.m_data[1] = static_cast<uint8_t>(rpm >> 16);
+  f.m_data[2] = static_cast<uint8_t>(rpm >> 8);
+  f.m_data[3] = static_cast<uint8_t>(rpm);
+  return f;
+}
+
+ServoFrame ServoFrame::setPosition(canid_t can_id, int32_t pos) {
+  ServoFrame f(can_id, ServoFrameID::PositionMode);
+
+  if (pos < -360000000 || pos > 360000000) {
+    char buf[500]{};
+    ::snprintf(buf, sizeof(buf) - 1,
+               "Invalid argument, you sent a value of %d but the limits are -360000000, 360000000",
+               pos);
+
+    throw std::invalid_argument(buf);
+  }
+
+  f.m_data[0] = static_cast<uint8_t>(pos >> 24);
+  f.m_data[1] = static_cast<uint8_t>(pos >> 16);
+  f.m_data[2] = static_cast<uint8_t>(pos >> 8);
+  f.m_data[3] = static_cast<uint8_t>(pos);
+
+  return f;
+}
+
+ServoFrame ServoFrame::setOrigin(canid_t can_id, uint8_t origin_mode) {
+  ServoFrame f(can_id, ServoFrameID::SetOriginMode);
+  if (origin_mode > 1) {
+    char buf[500]{};
+    ::snprintf(buf, sizeof(buf) - 1,
+               "Invalid argument, you sent a value of %d but the limits are 0 or 1", origin_mode);
+
+    throw std::invalid_argument(buf);
+  }
+  f.m_data[0] = origin_mode;
+  return f;
+}
+
+ServoFrame ServoFrame::setPositionAndVelo(canid_t can_id, int32_t position, int16_t speed,
+                                          int16_t accel) {
+  ServoFrame f(can_id, ServoFrameID::PositionVelocityMode);
+  if (position < -360000000 || position > 360000000) {
+    // TODO implement throw
+    char buf[500]{};
+    ::snprintf(buf, sizeof(buf) - 1,
+               "Invalid argument, you sent a value of %d but the limits are -360000000, 360000000",
+               position);
+
+    throw std::invalid_argument(buf);
+  }
+  // Speed just uses the int limit, no check needed
+
+  // Accel uses int limit as upper bound
+  if (accel < 0) {
+    char buf[500]{};
+    ::snprintf(buf, sizeof(buf) - 1,
+               "Invalid argument, you sent a value of %d but the limits are 0, 32767", position);
+
+    throw std::invalid_argument(buf);
+  }
+
+  f.m_data[0] = static_cast<uint8_t>(position >> 24);
+  f.m_data[1] = static_cast<uint8_t>(position >> 16);
+  f.m_data[2] = static_cast<uint8_t>(position >> 8);
+  f.m_data[3] = static_cast<uint8_t>(position);
+
+  f.m_data[4] = static_cast<uint8_t>(speed >> 8);
+  f.m_data[5] = static_cast<uint8_t>(speed);
+
+  f.m_data[6] = static_cast<uint8_t>(accel >> 8);
+  f.m_data[7] = static_cast<uint8_t>(accel);
+
+  return f;
+}
+
+uint8_t ServoFrame::get_id() { return static_cast<uint8_t>(m_frame_header >> 16); }
+
+bool ServoFrame::is0x29() {
+  if (get_id() == 0x29) {
+    return true;
+  }
+  return false;
+}
+
+float ServoFrame::getPosition() {
+  if (!is0x29()) {
+  }
+
+  int16_t pos{};
+  std::copy(m_data.begin(), m_data.begin() + 2, &pos);
+  /*
+   * Check ak-series PDF, protocol calls for this
+   * I want values to reflect what they look like on the motors
+   * not the explicit return values on the protocol,
+   * it introduces some confusion and makes life harder for anyone
+   * that doesnt have an intimate knowledge of the protocol
+   */
+  float posreal{static_cast<float>(pos)};
+  posreal /= 100;
+  return posreal;
+}
+
+int32_t ServoFrame::getSpeed() {
+  if (!is0x29()) {
+    throw aks::invalid_call(invalidCallStr(get_id()));
+  }
+
+  int16_t speed{};
+  std::copy(m_data.begin() + 2, m_data.begin() + 4, &speed);
+  int32_t speedreal{speed};
+  speedreal *= 10;
+  return speedreal;
+}
+
+float ServoFrame::getCurrent() {
+  if (!is0x29()) {
+    throw aks::invalid_call(invalidCallStr(get_id()));
+  }
+
+  int16_t current{};
+  std::copy(m_data.begin() + 4, m_data.begin() + 6, &current);
+  float currentreal{static_cast<float>(current)};
+  currentreal /= 100;
+  return currentreal;
+}
+
+int8_t ServoFrame::getTemperature() {
+  if (!is0x29()) {
+    throw aks::invalid_call(invalidCallStr(get_id()));
+  }
+
+  int8_t temp{static_cast<int8_t>(m_data[6])};
+  return temp;
+}
+
+ServoErrorCode ServoFrame::getErrorCode() {
+  if (!is0x29()) {
+    throw aks::invalid_call(invalidCallStr(get_id()));
+  }
+
+  ServoErrorCode r = static_cast<ServoErrorCode>(m_data[7]);
+  return r;
+}
