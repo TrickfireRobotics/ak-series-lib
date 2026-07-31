51bd69ffce293e71035a51926fbe9a32a5fab36a
CPrutean <calin.prutean@gmail.com>
2026-07-30 18:49:05 -0700

    feat: finished mit and servo mode bindings

--- include/AKSeries.hpp
+++ include/AKSeries.hpp
@@ -2,9 +2,11 @@
 #define __AK_SERIES_INTERFACE
 #pragma once
 #include <Errors.hpp>
+#include <can/MIT_frame.hpp>
 #include <can/comms.hpp>
 #include <memory>
 #include <motors/Motors.hpp>
+#include <optional>
 #include <stdint.h>
 
 namespace AKSeries {
@@ -13,29 +15,38 @@ class AKSeriesInterface;
 
 class Motor {
   friend class AKSeriesInterface;
+
+protected:
   std::shared_ptr<CanInterface> mInterface;
-  bool mMitMode;
-  template <typename T>
-  Motor(T motor, uint32_t canId, std::shared_ptr<CanInterface> interface, bool mitMode = true);
-  MotorRunLimits lims;
-  AKSeriesMotor motor;
+  Motor(AKSeriesMotor motor, uint32_t canId, std::shared_ptr<CanInterface> interface);
+  Motor(const MotorRunLimits *motor, uint32_t canId, std::shared_ptr<CanInterface> interface);
+  const MotorRunLimits *mLims;
+  uint32_t mCanId;
 
 public:
-  // TODO finish writing all servo and Mit mode bindings
-  bool isMitMode() { return mMitMode; }
-  // MITMode bindings
-  can_frame sendAndReceiveMit(MitRunSettings &m);
-  void sendMit(MitRunSettings &m);
-  // ServoModeBindings
-
   Motor() = delete;
-  // Object should be moved
   Motor(const Motor &) = delete;
   Motor &operator=(const Motor &) = delete;
-  Motor(Motor &&) noexcept;
-  Motor &operator=(Motor &&) noexcept;
-  // Should properly decrement refcount
-  ~Motor();
+  Motor(Motor &&) noexcept = default;
+  Motor &operator=(Motor &&) noexcept = default;
+  virtual ~Motor() = default;
+};
+
+class MitModeMotor : public Motor {
+public:
+  [[nodiscard]] std::optional<MitRecvFrame> sendAndRecieve(MitRunSettings &);
+  void send(MitRunSettings &);
+};
+
+class ServoModeMotor : public Motor {
+public:
+  void sendDutyCycle(float dutyCycle);
+  void sendCurrentLoop(float currentLoop);
+  void sendCurrentBrake(float current);
+  void sendRPM(float rpm);
+  void sendPosition(float pos);
+  void sendOrigin(uint8_t origin_mode);
+  void sendPositionAndVelo(float position, float speed, float accel);
 };
 
 class AKSeriesInterface {
@@ -44,12 +55,14 @@ class AKSeriesInterface {
 public:
   AKSeriesInterface(const char *canif);
   AKSeriesInterface() = delete;
-  template <typename T> Motor createMotor(T motor, uint32_t, bool mitMode = true);
+  Motor createMotor(AKSeriesMotor motor, uint32_t);
+  Motor createMotor(const MotorRunLimits *motor, uint32_t);
 
   AKSeriesInterface(const AKSeriesInterface &);
   AKSeriesInterface &operator=(const AKSeriesInterface &);
   AKSeriesInterface(AKSeriesInterface &&) noexcept;
   AKSeriesInterface &operator=(AKSeriesInterface &&) noexcept;
+
   // Be careful NOT to delete the resource
   // Additionally shouldnt be allowed to delete itself if motors still exist in scope
   ~AKSeriesInterface();
--- src/AKSeries.cpp
+++ src/AKSeries.cpp
@@ -1,28 +1,115 @@
 #include "AKSeries.hpp"
+#include "can/MIT_frame.hpp"
 #include "motors/Motors.hpp"
+#include <can/Servo_frame.hpp>
 #include <can/comms.hpp>
+#include <stdexcept>
 
 using namespace AKSeries;
 AKSeriesInterface::AKSeriesInterface(const char *canif)
     : canInterface{std::shared_ptr<CanInterface>(new CanInterface(canif))} {}
 
-template <typename T> Motor AKSeriesInterface::createMotor(T motor, uint32_t canID, bool mitMode) {
-  static_assert(std::is_same_v<T, AKSeriesMotor> || std::is_same_v<T, MotorRunLimits>,
-                "Can only construct using AKSeriesMotor or MotorRunLimits objects");
-  Motor m(motor, canID, canInterface, mitMode);
+Motor AKSeriesInterface::createMotor(AKSeriesMotor motor, uint32_t canID) {
+  Motor m(motor, canID, canInterface);
+  return m;
+}
+Motor AKSeriesInterface::createMotor(const MotorRunLimits *motor, uint32_t canID) {
+  if (motor == nullptr) {
+    throw std::invalid_argument("Invalid argument passed to create motor object, please pass a "
+                                "valid pointer `MotorRunLimits` object");
+  }
+  Motor m(motor, canID, canInterface);
   return m;
 }
 
-//
-template <typename T>
-Motor::Motor(T m, uint32_t canId, std::shared_ptr<CanInterface> interface, bool mitMode)
-    : mInterface{interface}, mMitMode{mitMode} {
-  // TODO finish template programming
-  if constexpr (std::is_same_v<T, AKSeriesMotor>) {
-    //
-  } else if constexpr (std::is_same_v<T, MotorRunLimits>) {
-    //
-  } else {
-    static_assert(!sizeof(T *), "unsupported type");
+Motor::Motor(AKSeriesMotor motor, uint32_t canId, std::shared_ptr<CanInterface> interface)
+    : mInterface{interface}, mLims{&motorRunLimits[static_cast<uint8_t>(motor)]}, mCanId{canId} {}
+
+Motor::Motor(const MotorRunLimits *lims, uint32_t canId, std::shared_ptr<CanInterface> interface)
+    : mInterface{interface}, mLims{lims}, mCanId{canId} {}
+
+// TODO finish implementing both MIT and servo mode binding
+
+std::optional<MitRecvFrame> MitModeMotor::sendAndRecieve(MitRunSettings &settings) {
+  MitSendFrame f(static_cast<canid_t>(mCanId), *this->mLims, &settings);
+  can_frame sf = static_cast<can_frame>(f);
+
+  auto recv = this->mInterface->sendAndRead(sf);
+
+  if (!recv.successful) {
+    std::fprintf(stderr, "IOError in the received can frame, logging");
+    return std::nullopt;
+  }
+  return MitRecvFrame(recv.success, *this->mLims);
+}
+void MitModeMotor::send(MitRunSettings &settings) {
+  MitSendFrame f(static_cast<canid_t>(mCanId), *this->mLims, &settings);
+  can_frame sf = static_cast<can_frame>(f);
+  CanIOError err = this->mInterface->send(sf);
+  if (err != CanIOError::NONE) {
+    std::fprintf(stderr, "IOError in the received can frame, logging");
+  }
+}
+
+void ServoModeMotor::sendDutyCycle(float dutyCycle) {
+  ServoSendFrame f = ServoSendFrame::setDutyCycle(mCanId, dutyCycle);
+  can_frame sf = static_cast<can_frame>(f);
+  CanIOError err = this->mInterface->send(sf);
+  if (err != CanIOError::NONE) {
+    std::fprintf(stderr, "IOError in the received can frame, logging");
+  }
+}
+
+void ServoModeMotor::sendCurrentLoop(float currentLoop) {
+  ServoSendFrame f = ServoSendFrame::setCurrentLoop(mCanId, currentLoop);
+  can_frame sf = static_cast<can_frame>(f);
+  CanIOError err = this->mInterface->send(sf);
+  if (err != CanIOError::NONE) {
+    std::fprintf(stderr, "IOError in the received can frame, logging");
+  }
+}
+
+void ServoModeMotor::sendCurrentBrake(float currentBrake) {
+  ServoSendFrame f = ServoSendFrame::setCurrentBrake(mCanId, currentBrake);
+  can_frame sf = static_cast<can_frame>(f);
+  CanIOError err = this->mInterface->send(sf);
+  if (err != CanIOError::NONE) {
+    std::fprintf(stderr, "IOError in the received can frame, logging");
+  }
+}
+
+void ServoModeMotor::sendRPM(float rpm) {
+  ServoSendFrame f = ServoSendFrame::setRPM(mCanId, rpm);
+  can_frame sf = static_cast<can_frame>(f);
+  CanIOError err = this->mInterface->send(sf);
+  if (err != CanIOError::NONE) {
+    std::fprintf(stderr, "IOError in the received can frame, logging");
+  }
+}
+
+void ServoModeMotor::sendPosition(float pos) {
+  ServoSendFrame f = ServoSendFrame::setPosition(mCanId, pos);
+  can_frame sf = static_cast<can_frame>(f);
+  CanIOError err = this->mInterface->send(sf);
+  if (err != CanIOError::NONE) {
+    std::fprintf(stderr, "IOError in the received can frame, logging");
+  }
+}
+
+void ServoModeMotor::sendOrigin(uint8_t originMode) {
+  ServoSendFrame f = ServoSendFrame::setOrigin(mCanId, originMode);
+  can_frame sf = static_cast<can_frame>(f);
+  CanIOError err = this->mInterface->send(sf);
+  if (err != CanIOError::NONE) {
+    std::fprintf(stderr, "IOError in the received can frame, logging");
+  }
+}
+
+void ServoModeMotor::sendPositionAndVelo(float pos, float speed, float accel) {
+  ServoSendFrame f = ServoSendFrame::setPositionAndVelo(mCanId, pos, speed, accel);
+  can_frame sf = static_cast<can_frame>(f);
+  CanIOError err = this->mInterface->send(sf);
+  if (err != CanIOError::NONE) {
+    std::fprintf(stderr, "IOError in the received can frame, logging");
   }
 }
